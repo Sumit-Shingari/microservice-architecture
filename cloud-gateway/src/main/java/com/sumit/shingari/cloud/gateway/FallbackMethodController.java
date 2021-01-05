b6e0af468f5f72844f6b0020661cd62794fb34a1
Sumit-Shingari <70474203+Sumit-Shingari@users.noreply.github.com>
2021-01-05 23:25:30 +0530

    Add files via upload

---
+++ cloud-gateway/src/main/java/com/sumit/shingari/cloud/gateway/FallbackMethodController.java
@@ -0,0 +1,19 @@
+package com.sumit.shingari.cloud.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackMethodController {
+	
+	@GetMapping("/userServiceFallback")
+	public String userServiceFallbackMethod() {
+		return "User Service is taking longer than expected, please try after some time !";
+	}
+	
+	@GetMapping("/departmentServiceFallback")
+	public String departmentServiceFallbackMethod() {
+		return "Department Service is taking longer than expected, please try after some time !";
+	}
+
+}
